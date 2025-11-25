b7fe5aca5ffb1bc25d7a648cc071f389efb7d002
220777316+TomasZ4Z@users.noreply.github.com <tomas@Tomas.myguest.virtualbox.org>
2025-11-25 18:32:18 -0300

    comenzando el proyecto: creacion de funciones basi cas de la calculadora

---
+++ script.js
@@ -0,0 +1,29 @@
+//Tu calculadora contendrá funciones para todos los operadores matemáticos básicos que suelen encontrarse
+//  en calculadoras, así que empieza creando funciones para los siguientes elementos y pruébalas en la consola de tu navegador:
+
+//sumar
+//restar
+//multiplicar
+//dividir
+// Tu calculadora contendrá funciones para todos los operadores matemáticos básicos que suelen encontrarse 
+// en calculadoras, así que empieza creando funciones para los siguientes elementos y pruébalas en la consola de tu navegador:
+
+function suma(a,b){
+return a + b
+}
+function restar(a,b){
+return a - b
+}
+function multiplicar(a,b){
+return a * b
+}
+function dividir(a,b){
+return a / b
+}
+
+firstValue = document.querySelector('.firstvalue')
+
+
+
+
+console.log(dividir(5,2))
