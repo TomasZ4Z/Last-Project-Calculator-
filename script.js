04c55f10d349f55db5eb0440ac4dcd08b4e897e6
220777316+TomasZ4Z@users.noreply.github.com <tomas@Tomas.myguest.virtualbox.org>
2025-11-27 18:47:39 -0300

    calculadora part 2

--- script.js
+++ script.js
@@ -8,22 +8,57 @@
 // Tu calculadora contendrá funciones para todos los operadores matemáticos básicos que suelen encontrarse 
 // en calculadoras, así que empieza creando funciones para los siguientes elementos y pruébalas en la consola de tu navegador:
 
-function suma(a,b){
-return a + b
+
+function mathematicalOperation(num1,operator,num2){
+if (typeof num1=="number" && typeof num2=="number" && operator === "+") {
+    return num1 + num2
 }
-function restar(a,b){
-return a - b
+else if (typeof num1=="number" && typeof num2=="number" && operator === "-") {
+    return num1 - num2
 }
-function multiplicar(a,b){
-return a * b
+else if (typeof num1=="number" && typeof num2=="number" && operator === "x") {
+    return num1 * num2
 }
-function dividir(a,b){
-return a / b
+else if (typeof num1=="number" && typeof num2=="number" && operator === "%") {
+    return num1 / num2
+}
+}
+console.log(mathematicalOperation(10,"%",5))
+
+
+
+
+
+
+const firstValue = document.querySelector("#user")
+
+
+const result = document.createElement("div")
+result.textContent = "hola"
+document.body.appendChild(result)
+
+function prueba(valor){
+ console.log('valor recibido es :', valor)
 }
 
-firstValue = document.querySelector('.firstvalue')
+equal = document.getElementById("equal")
+equal.addEventListener("click", () => {
+data = document.getElementById("user").value
+prueba(data)
+let separacion = data.split("")
+console.log(separacion)
+})
+let nombre = "thomas558+7"
+
+
+function array(){
+    
+}
 
 
 
 
-console.log(dividir(5,2))
+
+
+
+
